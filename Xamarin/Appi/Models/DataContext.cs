5b7bbc01f25127835c3df93dbdefcb4b933eda8e
BryanGonCa <bryangonca@gmail.com>
2020-01-03 20:52:14 -0400

    first commit

---
+++ Xamarin/Appi/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace apiPhoneBook.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Appi.Models.Usuario> Users { get; set; }
+    }
+}
